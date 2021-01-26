b85492fec9a40e621fd977ea80d52afb5f16c49f
kennethlember <kenneth.lember@ametikool.ee>
2021-01-26 09:59:38 +0200

    trololol sorting

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -10,39 +10,39 @@
 
 <body class="flex justify-center items-center h-full">
     <div>
-        <div class="flex justify-between">
+        <div class="flex justify-between border-b-2 border-black-bg-gray-100">
             <div class="p-4">Raamatu nimi:</div>
             <div class="p-4">{{$books -> title}}</div>
         </div>
-        <div class="flex justify-between">
+        <div class="flex justify-between border-b-2 border-black-bg-gray-100">
             <div class="p-4">Raamatu aasta:</div>
             <div class="p-4">{{$books -> release_date}}</div>
         </div>
-        <div class="flex justify-between">
+        <div class="flex justify-between border-b-2 border-black-bg-gray-100">
             <div class="p-4">Raamatu keel:</div>
             <div class="p-4">{{$books -> language}}</div>
         </div>
-        <div class="flex justify-between">
+        <div class="flex justify-between border-b-2 border-black-bg-gray-100">
             <div class="p-4">Raamatu kaanepilt:</div>
             <div class="p-4"><img src="{{$books -> cover_path}}" alt=""></div>
         </div>
-        <div class="flex justify-between">
+        <div class="flex justify-between border-b-2 border-black-bg-gray-100">
             <div class="p-4">Raamatu sisu:</div>
             <div class="p-4">{{$books -> summary}}</div>
         </div>
-        <div class="flex justify-between">
+        <div class="flex justify-between border-b-2 border-black-bg-gray-100">
             <div class="p-4">Raamatu hind:</div>
             <div class="p-4">{{$books -> price}}</div>
         </div>
-        <div class="flex justify-between">
+        <div class="flex justify-between border-b-2 border-black-bg-gray-100">
             <div class="p-4">Raamatud laos:</div>
             <div class="p-4">{{$books -> stock_saldo}}</div>
         </div>
-        <div class="flex justify-between">
+        <div class="flex justify-between border-b-2 border-black-bg-gray-100">
             <div class="p-4">Lehekülgi:</div>
             <div class="p-4">{{$books -> pages}}</div>
         </div>
-        <div class="flex justify-between">
+        <div class="flex justify-between border-b-2 border-black-bg-gray-100">
             <div class="p-4">Raamatud tüüp:</div>
             <div class="p-4">{{$books -> type}}</div>
         </div>
